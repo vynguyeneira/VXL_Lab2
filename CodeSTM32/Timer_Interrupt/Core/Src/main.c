89e1ee299e6ec4b75e35006208fe8bdfacb80332
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-10-17 21:42:58 +0700

    Commit exercise 06 - Software timer is checked

--- CodeSTM32/Timer_Interrupt/Core/Src/main.c
+++ CodeSTM32/Timer_Interrupt/Core/Src/main.c
@@ -56,6 +56,27 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;  //10ms
+
+void setTimer0(int duration)
+{
+	timer0_counter = duration / TIMER_CYCLE;
+	timer0_flag = 0;
+}
+
+void timer_run()
+{
+	if(timer0_counter > 0)
+	{
+		timer0_counter--;
+		if(timer0_counter == 0) timer0_flag = 1;
+	}
+}
+
+
+
 const int MAX_LED = 4;
 int index_led = 0;  //from 0 to 3
 int led_buffer[4] = {2, 3, 4, 5};
@@ -206,9 +227,17 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
+  setTimer0(1000);
+
   hour = 15, minute = 8, second = 50;
   while (1)
   {
+	  if(timer0_flag == 1)
+	  {
+		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+		  setTimer0(2000);
+	  }
+
 	  second++;
 	  if(second >= 60){
 		  second = 0;
@@ -366,12 +395,14 @@ int counter_for_switch_LED7SEG = 50;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	counter_for_led_red_blinky--;
-	if (counter_for_led_red_blinky <= 0) //toggle LED RED (PA5) every second
-	{
-		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-		counter_for_led_red_blinky = 100;
-	}
+	timer_run();
+
+//	counter_for_led_red_blinky--;
+//	if (counter_for_led_red_blinky <= 0) //toggle LED RED (PA5) every second
+//	{
+//		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+//		counter_for_led_red_blinky = 100;
+//	}
 
 	counter_for_DOT--;
 	if(counter_for_DOT <= 0)  //Blink the two LEDs every second
